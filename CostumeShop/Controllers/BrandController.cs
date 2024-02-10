5ac7132ad21c9cbf57a0876d591ad687a808fb82
Bunyan33 <sam.ally33@gmail.com>
2024-02-11 02:40:43 +0530

    Edit view and delete view created notification popup included

--- CostumeShop/Controllers/BrandController.cs
+++ CostumeShop/Controllers/BrandController.cs
@@ -1,6 +1,7 @@
 ﻿using CostumeShop.Data;
 using CostumeShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CostumeShop.Controllers
 {
@@ -18,7 +19,7 @@ namespace CostumeShop.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            List<Brand> brands = _dbContext.Brand.ToList();
+            IEnumerable<Brand> brands = _dbContext.Brand.ToList();
             return View(brands);
         }
 
@@ -55,6 +56,8 @@ namespace CostumeShop.Controllers
             {
                 _dbContext.Brand.Add(brand);
                 _dbContext.SaveChanges();
+
+                TempData["success"] = "Record Created Successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View();
@@ -74,6 +77,97 @@ namespace CostumeShop.Controllers
             Brand brand = _dbContext.Brand.FirstOrDefault(x => x.Id == id);
 
             return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            var file = HttpContext.Request.Form.Files;
+
+            if (file.Count > 0)
+            {
+                string newFileName = Guid.NewGuid().ToString();
+
+                var upload = Path.Combine(webRootPath, @"images\brand");
+
+                var extention = Path.GetExtension(file[0].FileName);
+
+
+                var objFromDb = _dbContext.Brand.AsNoTracking().FirstOrDefault(x => x.Id == brand.Id);
+
+                if (objFromDb.BrandLogo != null)
+                {
+                    var oldImagePath = Path.Combine(webRootPath, objFromDb.BrandLogo.Trim('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+
+                }
+
+                using (var fileStream = new FileStream(Path.Combine(upload, newFileName + extention), FileMode.Create))
+                {
+                    file[0].CopyTo(fileStream);
+                }
+
+                brand.BrandLogo = @"\images\brand\" + newFileName + extention;
+            }
+
+            if (ModelState.IsValid)
+            {
+                var objFromDb = _dbContext.Brand.AsNoTracking().FirstOrDefault(x => x.Id == brand.Id);
+
+                objFromDb.Name = brand.Name;
+                objFromDb.EstablishedYear = brand.EstablishedYear; ;
+
+                if(brand.BrandLogo != null)
+                {
+                    objFromDb.BrandLogo = brand.BrandLogo;
+                }
+
+                _dbContext.Brand.Update(brand);
+                _dbContext.SaveChanges();
+                TempData["warning"] = "Record Edited Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            Brand brand = _dbContext.Brand.FirstOrDefault(x => x.Id == id);
+
+            return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Brand brand)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            var objFromDb = _dbContext.Brand.AsNoTracking().FirstOrDefault(x => x.Id == brand.Id);
+
+            if (!string.IsNullOrEmpty(brand.BrandLogo))
+            {
+                if (objFromDb.BrandLogo != null)
+                {
+                    var oldImagePath = Path.Combine(webRootPath, objFromDb.BrandLogo.Trim('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+
+                }
+            }
+            _dbContext.Brand.Remove(brand);
+            _dbContext.SaveChanges();
+            TempData["error"] = "Record Deleted Successfully";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
